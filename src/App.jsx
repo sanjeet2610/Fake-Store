d9a6343ab68c1342950d2ecce8aa4d6b14781bf8
sanjeet2610 <195545915+sanjeet2610@users.noreply.github.com>
2026-04-06 15:08:27 +0000

    create cart context and generate products data

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Cart from "./pages/cart/Cart";
-import { useContext } from "react";
 import { useCartContext } from "./context/CartContext";
 const App = () => {
   const { products } = useCartContext();
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,10 +1,20 @@
 import { createContext, useContext, useState } from "react";
+import { faker } from "@faker-js/faker";
 
 const CartContext = createContext();
 
 const CartContextProvider = ({ children }) => {
-  const [products, setProducts] = useState(["a"]);
-
+  const products = new Array(20).fill(null).map(() => ({
+    id: faker.string.uuid(),
+    productName: faker.commerce.productName(),
+    productDescription: faker.commerce.productDescription(),
+    price: faker.commerce.price({ min: 100, max: 5000 }),
+    image: faker.image.urlPicsumPhotos({ height: 300, width: 300 }),
+    inStock: faker.helpers.arrayElement([0, 5, 14, 16, 23]),
+    fastDelivery: faker.datatype.boolean(),
+    new: faker.datatype.boolean(),
+    ratings: faker.helpers.arrayElement([1, 2, 3, 4, 5]),
+  }));
   const state = {
     products,
   };
